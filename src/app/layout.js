c3dcc8830da3d45d49b4b2a745ce041aa7e7d3c2
Surafel Kassahun <surafelkassahun21@gmail.com>
2024-10-25 18:43:14 +0300

    update the meta datas

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-// app/layout.js or app/_app.js
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
@@ -15,13 +14,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-          <title>Surafel - Full Stack Developer</title>
-          <meta name="description" content="Full Stack Developer skilled in building dynamic web applications. Check out my projects, skills, and passion for technology!" />                
+      <head>
+        <title>Surafel - Full Stack Developer</title>
+        <meta name="description" content={metadata.description} />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      <body className={roboto.className} style={{ backgroundColor: '#051622' }} >
-        <main className="w-full mx-auto p-4"  >
+      </head>
+      <body className={roboto.className} style={{ backgroundColor: '#051622' }}>
+        <main className="w-full mx-auto p-4">
           {children}
         </main>
+        <footer className="text-center p-4">
+          <p>
+            Created by <a href="https://surafelkportfolio.vercel.app/" className="text-blue-500">Surafel</a>
+          </p>
+        </footer>
       </body>
     </html>
   );
